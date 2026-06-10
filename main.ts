9f90831044ba71fd126ef8ade0c5510dfcb0993a
Ezekiel Olanrewaju <Olanrewajuezekiel121@gmail.com>
2026-06-10 21:10:32 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -131,7 +131,7 @@ app.onError((err, c) => {
 // Insure security - The autograder will have you change this value
 function checkToken(c) {
   const token = c.req.query("token");
-  if ( token == '42' ) return true;
+  if ( token == '2609_4fb218:3f92c3' ) return true;
   throw new HTTPException(401, { message: 'Missing or invalid token' }); 
 }
 
